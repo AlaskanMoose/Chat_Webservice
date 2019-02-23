5bbc1984e9e851941d455ad58e0abec51af19458
Armoni <athera@uw.edu>
2019-02-22 20:52:30 -0800

    Added middleware back

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const pgp = require("pg-promise")();
 pgp.pg.defaults.ssl = true;
 app.use("/login", require("./routes/login.js"));
 app.use("/register", require("./routes/register.js"));
-app.use('/pushy', require('./routes/pushy.js'));
+// app.use('/pushy', require('./routes/pushy.js'));
+app.use('/pushy', middleware.checkToken, require('./routes/pushy.js'));
 
 /*
  * Return HTML for the / end point.
